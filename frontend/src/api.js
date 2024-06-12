79c63a84a8dc46ef5f2e5565f3bd290903406722
KentonMewling <tjwright1228@gmail.com>
2024-06-12 11:28:49 -0700

    fixed connection issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,8 +5,8 @@ const apiUrl = "/choreo-apis/djr-grocery/backend/v1";
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.VITE_API_URL : apiUrl,
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
     (config) => {
